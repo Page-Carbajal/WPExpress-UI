d8e42998c62f18bb9e295478882e8052f4b57c8f
awesomepage <page@iconicamarketing.com>
2016-01-13 17:14:30 -0500

    Version 0.5 untested

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # WPExpress-UI
 
-HTML Builder and Render Engine for WPExpress. 
+HTML Builder and Render Engine for WPExpress.
+ 
+
+##Change Log
+
+
+###TO DO
+
+
+- Create Formatting class a port of WordPress functions to sanitize titles and file names
+
+###Version 0.5
+
+- Made the RenderEngine class final
+- Simplified the constructor
+- Added two error throwing exceptions
+- Added the RenderEngine/setTemplatePath method 
+- Added the RenderEngine/createDirectoryStructure method
+- Remove WordPress specific functions dependencies
+
+###Version 0.4
+
+- Last stable version
--- source/UI/RenderEngine.class.php
+++ source/UI/RenderEngine.class.php
@@ -11,7 +11,7 @@ use Twig_Environment;
 
 
 
-class RenderEngine
+final class RenderEngine
 {
     protected $type;
     protected $typeExtension;
@@ -19,20 +19,23 @@ class RenderEngine
     protected $templateFolder;
     protected $useTypeAsExtension;
 
-    public function __construct($templateFolderPath, $type = 'mustache')
+    public function __construct($templateFolderPath, $type = 'mustache', $useTypeAsExtension = true)
     {
         $this->type = trim(strtolower($type));
-        $this->useTypeAsExtension = true;
-        $this->typeExtension = sanitize_title($this->type);
+        $this->useTypeAsExtension = $useTypeAsExtension;
+        $this->typeExtension = sanitize_title($this->type); // TODO: Port this function to Plain PHP
+        $this->setTemplatePath( $templateFolderPath )->createDirectoryStructure();
+    }
 
-        if($templateFolderPath !== false && file_exists( $templateFolderPath ) ){
+    public function setTemplatePath($templateFolderPath)
+    {
+        if( ( !empty($templateFolderPath) ) && file_exists( $templateFolderPath ) ){
             $this->templateFolder = $templateFolderPath;
         }else{
-            throw new \Exception( __('No such folder! - WPExpress @ Render Engine', 'wpexpress') );
+            throw new \Exception( 'Error: Template path does not exist! - WPExpress/UI @ RenderEngine' );
         }
 
-        $this->createDirectoryStructure();
-
+        return $this;
     }
 
     public function setTypeAsExtension($flag)
@@ -42,26 +45,95 @@ class RenderEngine
 
     private function createDirectoryStructure()
     {
-        switch( $this->type ){
-            case "twig":
-                // Create the directory <path-to-template>/twig/cache
-                $partialsPath = $this->getBaseDirectory() . '/cache/';
-                if( !file_exists( $partialsPath ) ){
-                    wp_mkdir_p( $partialsPath );
-                }
-                break;
-            default:
-                // Create the directory <path-to-template>/mustache/partials
-                $partialsPath = $this->getBaseDirectory() . '/partials/';
-                if( !file_exists( $partialsPath ) ){
-                    wp_mkdir_p( $partialsPath );
-                }
-                $cachePath = $this->getBaseDirectory() . '/cache/';
-                if( !file_exists( $cachePath ) ){
-                    wp_mkdir_p( $cachePath );
-                }
-                break;
+        $success = true;
+
+        // Create the directory <path-to-template>/mustache/partials
+        $partialsPath = $this->getBaseDirectory() . '/partials/';
+        if( !file_exists( $partialsPath ) ){
+            $success = $this->makeDirectoryFromPath( $partialsPath );
         }
+        $cachePath = $this->getBaseDirectory() . '/cache/';
+        if( !file_exists( $cachePath ) ){
+            $success = $this->makeDirectoryFromPath( $cachePath );
+        }
+
+        if(!$success){
+            throw new \Exception( 'Error: Cant write in the specified template path. Check for permissions. - WPExpress/UI @ RenderEngine' );
+        }
+
+        return $this;
+    }
+
+    // This function is a verbatim copy of WordPress wp_is_stream
+    private function isURLStream( $path )
+    {
+        $wrappers = stream_get_wrappers();
+        $wrappers_re = '(' . join('|', $wrappers) . ')';
+
+        return preg_match( "!^$wrappers_re://!", $path ) === 1;
+    }
+
+    // This function is a verbatim copy of WordPress wp_mkdir_p
+    private function makeDirectoryFromPath($target )
+    {
+
+        $wrapper = null;
+
+        // Strip the protocol.
+        if( $this->isURLStream( $target ) ) {
+            list( $wrapper, $target ) = explode( '://', $target, 2 );
+        }
+
+        // From php.net/mkdir user contributed notes.
+        $target = str_replace( '//', '/', $target );
+
+        // Put the wrapper back on the target.
+        if( $wrapper !== null ) {
+            $target = $wrapper . '://' . $target;
+        }
+
+        /*
+         * Safe mode fails with a trailing slash under certain PHP versions.
+         * Use rtrim() instead of untrailingslashit to avoid formatting.php dependency.
+         */
+        $target = rtrim($target, '/');
+        if ( empty($target) )
+            $target = '/';
+
+        if ( file_exists( $target ) )
+            return @is_dir( $target );
+
+        // We need to find the permissions of the parent folder that exists and inherit that.
+        $target_parent = dirname( $target );
+        while ( '.' != $target_parent && ! is_dir( $target_parent ) ) {
+            $target_parent = dirname( $target_parent );
+        }
+
+        // Get the permission bits.
+        if ( $stat = @stat( $target_parent ) ) {
+            $dir_perms = $stat['mode'] & 0007777;
+        } else {
+            $dir_perms = 0777;
+        }
+
+        if ( @mkdir( $target, $dir_perms, true ) ) {
+
+            /*
+             * If a umask is set that modifies $dir_perms, we'll have to re-set
+             * the $dir_perms correctly with chmod()
+             */
+            if ( $dir_perms != ( $dir_perms & ~umask() ) ) {
+                $folder_parts = explode( '/', substr( $target, strlen( $target_parent ) + 1 ) );
+                for ( $i = 1; $i <= count( $folder_parts ); $i++ ) {
+                    @chmod( $target_parent . '/' . implode( '/', array_slice( $folder_parts, 0, $i ) ), $dir_perms );
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+
     }
 
     public function getBaseDirectory()
@@ -105,12 +177,15 @@ class RenderEngine
     private function renderMustacheTemplate($fileName, $context)
     {
         $options = array();
-//        $options['cache'] = $this->getBaseDirectory() . '/cache';
+        $options['cache'] = $this->getBaseDirectory() . '/cache';
         $options['loader'] = new Mustache_Loader_FilesystemLoader( $this->getBaseDirectory() );
         $options['partials_loader'] = new Mustache_Loader_FilesystemLoader( $this->getBaseDirectory() . '/partials' );
         $options['charset'] = 'UTF-8';
 
-        $options = apply_filters( 'wpex_set_mustache_engine_options', $options );
+        if( function_exists( 'apply_filters' ) ){
+            // Prevent the use of WordPress specific functions
+            $options = apply_filters( 'wpex_set_mustache_engine_options', $options );
+        }
 
         $mustache = new Mustache_Engine($options);
 
